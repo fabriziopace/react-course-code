042ef1dfafb20febc348cc67c4c2028280cfc27b
fabriziopace <pacefabriziowork@gmail.com>
2020-01-05 10:44:53 +0100

    Styling with Stylesheets

--- projects/react-complete-guide/src/App.js
+++ projects/react-complete-guide/src/App.js
@@ -33,11 +33,21 @@ const app = props => {
     });
   }
 
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={() => switchNameHandler('Fabrizio!!')}>Switch Name</button>
+      <button 
+        style={style}
+        onClick={() => switchNameHandler('Fabrizio!!')}>Switch Name</button>
       <Person
         name={personsState.persons[0].name}
         age={personsState.persons[0].age} />
