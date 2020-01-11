0440c2ba224b68e4b0b0650f3f1b878b7a1215a1
fabriziopace <pacefabriziowork@gmail.com>
2020-01-11 22:22:31 +0100

    Styled component and dynamic styles

--- projects/react-complete-guide/src/App.js
+++ projects/react-complete-guide/src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`
 
 class App extends Component {
   state = {
@@ -81,18 +95,14 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App" >
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Person</button>
-          {persons}
-        </div >
-      </StyleRoot>
+      <div className="App" >
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Person</StyledButton>
+        {persons}
+      </div >
     );
   }
 };
 
-export default Radium(App);
+export default App;
