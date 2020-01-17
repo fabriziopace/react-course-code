e16443ff8860dfd48c15e8ba3e67a3616c3b36f6
fabriziopace <pacefabriziowork@gmail.com>
2020-01-17 12:10:06 +0100

    Readme file updated

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Code of React - The Complete Guide Course
 1. First React Code
 1. ES7 JavaScript Refresher
 3. First React Create App
+4. React Complete Guide
+5. React Practice Section 3
+6. React Practice Section 4
--- projects/react-complete-guide/src/components/Persons/Persons.js
+++ projects/react-complete-guide/src/components/Persons/Persons.js
@@ -44,16 +44,11 @@ class Persons extends PureComponent {
         console.log('[Person.js] rendering...');
         return this.props.persons.map((person, index) => {
             return <Person
-            click = {
-                () => this.props.clicked(index)
-            }
-            name = { person.name }
-            age = { person.age }
-            key = { person.id }
-            changed = {
-                (event) => this.props.changed(event, person.id)
-            }
-            />
+                click={() => this.props.clicked(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.props.changed(event, person.id)} />
         });
     }
 };
