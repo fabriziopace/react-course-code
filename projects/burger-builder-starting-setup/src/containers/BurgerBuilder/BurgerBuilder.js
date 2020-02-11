f3fe80b2a0f13eb43e172bfaabefa6a9f6feea30
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-02-11 18:02:42 +0100

    Burger Builder - Routing

--- projects/burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ projects/burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,7 +31,7 @@ class BurgerBuilder extends Component {
                 this.setState({ ingredients: response.data })
             })
             .catch(error => {
-                this.setState({error: true});
+                this.setState({ error: true });
             });
     }
     updatePurchaseState(ingredients) {
@@ -82,29 +82,17 @@ class BurgerBuilder extends Component {
     }
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Fabrizio Pace',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCode: '41239',
-        //             country: 'Italy'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // };
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     });
-        this.props.history.push('/checkout');
+
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
     render() {
         const disabledInfo = {
