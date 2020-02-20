25bc94a8725f0f2135a0425a30baccbc392a1e10
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-02-20 14:38:17 +0100

    Redux - Updating state immutably

--- projects/redux--01-start/src/store/reducer.js
+++ projects/redux--01-start/src/store/reducer.js
@@ -1,28 +1,35 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + 5
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - 5
-        }
-    }
+    switch (action.type) {
+        case 'INCREMENT':
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+        case 'SUBTRACT':
+            return {
+                ...state,
+                counter: state.counter - action.val
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({ id: Date.now(), value: state.counter })
+            }
+    };
     return state;
 };
 
