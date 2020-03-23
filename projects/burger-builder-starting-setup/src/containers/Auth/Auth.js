9504d2db5f31b49062c7c1f82c806f9b6d0bbeb4
fabriziopace <pacefabriziowork@gmail.com>
2020-03-23 20:31:07 +0100

    Burger Builder - Adding actions

--- projects/burger-builder-starting-setup/src/containers/Auth/Auth.js
+++ projects/burger-builder-starting-setup/src/containers/Auth/Auth.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
     state = {
@@ -65,7 +67,12 @@ class Auth extends Component {
                 touched: true
             }
         };
-        this.setState({controls: updatedControls});
+        this.setState({ controls: updatedControls });
+    };
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
     };
 
     render() {
@@ -90,7 +97,7 @@ class Auth extends Component {
         ))
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
@@ -98,5 +105,10 @@ class Auth extends Component {
         );
     }
 }
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth())
+    };
+};
 
-export default Auth;
+export default connect(null, mapDispatchToProps)(Auth);
