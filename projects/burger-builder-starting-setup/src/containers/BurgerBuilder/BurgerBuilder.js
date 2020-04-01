256785a579d62f34295afe938edab1597d732661
fabriziopace <pacefabriziowork@gmail.com>
2020-04-01 20:28:09 +0200

    Burger Builder - Forwarding unauthenticated users

--- projects/burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ projects/burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,7 +32,11 @@ class BurgerBuilder extends Component {
         return sum > 0;
     }
     purchaseHandler = () => {
-        this.setState({ purchasing: true });
+        if (this.props.isAuthenticated) {
+            this.setState({ purchasing: true });
+        } else {
+            this.props.history.push('/auth');
+        };
     }
     purchaseCancelHandler = () => {
         this.setState({ purchasing: false });
@@ -62,6 +66,7 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}
                         price={this.props.price} />
                 </Auxiliary>
             );
@@ -87,7 +92,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     }
 };
 
