e2e27e9df006a3dbbaf6ebe34df31b2be80799f8
fabriziopace <pacefabriziowork@gmail.com>
2020-04-02 20:36:46 +0200

    Burger Builder - Redirecting user to checkout page

--- projects/burger-builder-starting-setup/src/store/reducers/burgerBuilder.js
+++ projects/burger-builder-starting-setup/src/store/reducers/burgerBuilder.js
@@ -4,7 +4,8 @@ import { updateObject } from '../utility';
 const initialState = {
     ingredients: null,
     totalPrice: 4,
-    error: false
+    error: false,
+    building: false
 };
 
 const INGREDIENTS_PRICES = {
@@ -21,7 +22,8 @@ const addIngredient = (state, action) => {
     const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
     const updateState = {
         ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName],
+        building: true
     }
     return updateObject(state, updateState);
 };
@@ -33,7 +35,8 @@ const removeIngredient = (state, action) => {
     const updatedIngs = updateObject(state.ingredients, updatedIng);
     const updateSt = {
         ingredients: updatedIngs,
-        totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName],
+        building: true
     }
     return updateObject(state, updateSt);
 }
@@ -48,7 +51,8 @@ const setIngredients = (state, action) => {
             meat: action.ingredients.meat
         },
         totalPrice: 4,
-        error: false
+        error: false,
+        building: false
     });
 };
 
