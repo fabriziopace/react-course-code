480bbae2c804719f88259b850a99a98ed88b6895
unknown <pacefabriziowork@gmail.com>
2020-05-16 20:25:08 +0200

    Moving the Orders Side Effects into Sagas

--- projects/burger-builder-starting-setup/src/store/actions/index.js
+++ projects/burger-builder-starting-setup/src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addIngredient, removeIngredient, initIngredients, setIngredients, fetchIngredientsFailed } from './burgerBuilder';
-export { purchaseBurger, purchaseInit, fetchOrders } from './order';
+export { purchaseBurger, purchaseInit, fetchOrders, purchaseBurgerStart, purchaseBurgerFail, purchaseBurgerSuccess, fetchOrdersSuccess, fetchOrdersStart, fetchOrdersFail } from './order';
 export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, authStart, authSuccess, authFail, checkAuthTimeout } from './auth';
