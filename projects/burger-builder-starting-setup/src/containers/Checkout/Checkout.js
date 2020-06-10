02226ee6e3e145bf814dc8eeeaa09482897c977e
unknown <pacefabriziowork@gmail.com>
2020-06-10 23:12:21 +0200

    Adjusting the Order & Checkout Containers

--- projects/burger-builder-starting-setup/src/containers/Checkout/Checkout.js
+++ projects/burger-builder-starting-setup/src/containers/Checkout/Checkout.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 import { connect } from 'react-redux';
 
-class Checkout extends Component {
-    checkoutCancelledHandler = () => {
-        this.props.history.goBack();
+const checkout = props => {
+    const checkoutCancelledHandler = () => {
+        props.history.goBack();
     };
-    checkoutContinuedHandler = () => {
-        this.props.history.replace( '/checkout/contact-data' );
+
+    const checkoutContinuedHandler = () => {
+        props.history.replace('/checkout/contact-data');
     };
-    render() {
-        let summary = <Redirect to="/" />;
-        if (this.props.ings) {
-            const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
-            summary = (
-                <div>
-                    {purchasedRedirect}
-                    <CheckoutSummary
-                        ingredients={this.props.ings}
-                        checkoutCancelled={this.checkoutCancelledHandler}
-                        checkoutContinued={this.checkoutContinuedHandler} />
-                    <Route
-                        path={this.props.match.path + '/contact-data'}
-                        component={ContactData} />
-                </div>
-            );
-        };
-        return summary;
+
+    let summary = <Redirect to="/" />;
+    if (props.ings) {
+        const purchasedRedirect = props.purchased ? <Redirect to="/" /> : null;
+        summary = (
+            <div>
+                {purchasedRedirect}
+                <CheckoutSummary
+                    ingredients={props.ings}
+                    checkoutCancelled={checkoutCancelledHandler}
+                    checkoutContinued={checkoutContinuedHandler} />
+                <Route
+                    path={props.match.path + '/contact-data'}
+                    component={ContactData} />
+            </div>
+        );
     };
+    return summary;
 };
 
 const mapToStateToProps = state => {
@@ -39,4 +39,4 @@ const mapToStateToProps = state => {
     };
 };
 
-export default connect(mapToStateToProps)(Checkout);
+export default connect(mapToStateToProps)(checkout);
--- projects/burger-builder-starting-setup/src/containers/Orders/Orders.js
+++ projects/burger-builder-starting-setup/src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
@@ -6,31 +6,25 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
-class Orders extends Component {
-    state = {
-        orders: [],
-        loading: true
+const orders = props => {
+    useEffect(() => {
+        this.props.onFetchOrders(props.token, props.userId);
+    }, []);
+
+    let orders = <Spinner />;
+    if (!props.loading) {
+        orders = props.orders.map(order => (
+            <Order
+                key={order.id}
+                ingredients={order.ingredients}
+                price={order.price} />
+        ));
     }
-    componentDidMount() {
-        this.props.onFetchOrders(this.props.token, this.props.userId);
-    }
-    render() {
-        let orders = <Spinner />;
-        if (!this.props.loading) {
-            orders =
-                this.props.orders.map(order => (
-                    <Order
-                        key={order.id}
-                        ingredients={order.ingredients}
-                        price={order.price} />
-                ));
-        }
-        return (
-            <div>
-                {orders}
-            </div>
-        );
-    }
+    return (
+        <div>
+            {orders}
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
@@ -48,4 +42,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(orders, axios));
