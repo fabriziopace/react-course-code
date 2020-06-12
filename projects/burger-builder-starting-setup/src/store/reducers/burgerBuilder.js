1b3c41f0557ee8f10caf8d13ceeab19f27eb1d4a
unknown <pacefabriziowork@gmail.com>
2020-06-12 16:46:25 +0200

    Converting the BurgerBuilder Container

--- projects/burger-builder-starting-setup/src/store/reducers/burgerBuilder.js
+++ projects/burger-builder-starting-setup/src/store/reducers/burgerBuilder.js
@@ -35,7 +35,7 @@ const removeIngredient = (state, action) => {
     const updatedIngs = updateObject(state.ingredients, updatedIng);
     const updateSt = {
         ingredients: updatedIngs,
-        totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName],
+        totalPrice: state.totalPrice - INGREDIENTS_PRICES[action.ingredientName],
         building: true
     }
     return updateObject(state, updateSt);
