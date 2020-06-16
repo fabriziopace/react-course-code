d08e60d86362108b7021647aa53eb723181d53ab
unknown <pacefabriziowork@gmail.com>
2020-06-16 12:53:36 +0200

    Setting the right useEffect() Dependencies

--- projects/burger-builder-starting-setup/src/containers/Orders/Orders.js
+++ projects/burger-builder-starting-setup/src/containers/Orders/Orders.js
@@ -7,9 +7,12 @@ import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 const orders = props => {
+
+    const { onFetchOrders } = props;
+
     useEffect(() => {
-        this.props.onFetchOrders(props.token, props.userId);
-    }, []);
+        onFetchOrders(props.token, props.userId);
+    }, [onFetchOrders]);
 
     let orders = <Spinner />;
     if (!props.loading) {
